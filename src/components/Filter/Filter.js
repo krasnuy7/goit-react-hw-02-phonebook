cd23adf7cb8c41c2c0f69828c00e0e5cc3a371b9
unknown <you@example.com>
2020-06-23 00:32:13 +0300

    filter update

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 const Filter = (props) => {
+  console.log(props);
   return (
     <>
       <input
         name="filter"
         placeholder="seach"
         input="text"
+        value={props.filter}
         onChange={props.handleChange}
       />
     </>
